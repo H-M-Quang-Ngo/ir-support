31209c78f985b65647a1ae93d29e5418bbf43142
Gavin <gavin.paul-1@uts.edu.au>
2025-07-21 08:34:16 +1000

    Updated the supporting functions for the lab exercises 2 to 6, including placing fences, moving the robot cow, UFO objects and functions for switft, and various other minor supportive classes and functions.

--- ir_support/classes/EllipsoidRobot.py
+++ ir_support/classes/EllipsoidRobot.py
@@ -122,9 +122,19 @@ class EllipsoidRobot:
         return transforms
 
     # ---------------------------------------------------------------------------------------#
-    def teach(self):
+    def teach(self, on_update=None):
         """
-        Move the ellipsoids around with robot
+        Move the ellipsoids around with robot and optionally call a callback on each update.
+
+        Parameters
+        ----------
+        on_update : callable or None
+             A function to call every time the robot configuration changes.
+            It will be called with:
+                on_update(transforms, radii_list)
+            where:
+                transforms = list of 4x4 homogeneous transforms (one per link)
+                radii_list = list of [xr, yr, zr] radii for each link
         """
         self.fig._add_teach_panel(self.robot, self.robot.q)
         print("Teach mode. Press Enter to discard ellipsoids!")
@@ -140,15 +150,93 @@ class EllipsoidRobot:
         # Register the callback function with the keyboard listener
         keyboard.on_press_key("enter", on_enter_press)
 
+        # Track last joint configuration
+        last_q = np.copy(self.robot.q)
+
         # Main loop to update the ellipsoids
         while not stop_flag:
-            self.ellipsoid_for_robot_links(self.robot.q)
-            self.plot_ellipsoids()
-            self.fig.step(0.05)
+            self.fig.step(0.01)
+            current_q = np.copy(self.robot.q)
+            if not np.allclose(current_q, last_q):
+                last_q = current_q
+                self.ellipsoid_for_robot_links(self.robot.q)
+                self.plot_ellipsoids()
+                self.fig.step(0.05)
+                if on_update is not None:
+                    transforms = []
+                    radii_list = []
+                    for info in self.ellipsoid_matrices:
+                        shape_matrix = info['matrix']
+                        center_point = info['center']
+                        eigvals, eigvecs = np.linalg.eigh(shape_matrix)
+                        R = eigvecs
+                        radii = np.sqrt(eigvals)
+                        T = np.vstack((np.hstack((R, center_point.reshape(3,1))),
+                                    np.array([0,0,0,1])))
+                        transforms.append(T)
+                        radii_list.append(radii)
+                    on_update(transforms, radii_list)
 
         # Clean up
         self.remove_ellipsoid()
         keyboard.unhook_all()  # Unhook all keyboard listeners
+
+    # ---------------------------------------------------------------------------------------#
+    def get_ellipsoid_parameters(self, link_index: int):
+        """
+        Get center point, orientation matrix, and radii for a given link's ellipsoid.
+
+        Parameters
+        ----------
+        link_index : int
+            Index of the link (0 = first link, -1 = last link)
+
+        Returns
+        -------
+        center_point : np.ndarray
+            Translation vector [xc, yc, zc]
+        R : np.ndarray
+            3x3 orientation matrix (columns are principal axes)
+        radii : np.ndarray
+            Radii along the principal axes [xr, yr, zr]
+        """
+        if link_index < 0:
+            link_index = len(self.ellipsoid_matrices) + link_index
+        if link_index < 0 or link_index >= len(self.ellipsoid_matrices):
+            raise IndexError("link_index out of range")
+
+        info = self.ellipsoid_matrices[link_index]
+        center_point = info['center']
+        shape_matrix = info['matrix']
+
+        # Decompose shape matrix to orientation (R) and radii
+        eigvals, eigvecs = np.linalg.eigh(shape_matrix)
+        R = eigvecs                 # orientation matrix
+        radii = np.sqrt(eigvals)    # scales along those axes
+
+        return center_point, R, radii
+
+    # ---------------------------------------------------------------------------------------#    
+    def get_ellipsoid_transform_and_radii(self, link_index: int):
+        """
+        Get the 4x4 transformation matrix and radii for a given link's ellipsoid.
+
+        Parameters
+        ----------
+        link_index : int
+            Index of the link (0 = first link, -1 = last link)
+
+        Returns
+        -------
+        T : np.ndarray
+            4x4 transformation matrix
+        radii : np.ndarray
+            Radii along the principal axes [xr, yr, zr]
+        """
+        center_point, R, radii = self.get_ellipsoid_parameters(link_index)
+        T = np.vstack((np.hstack((R, center_point.reshape(3,1))),
+                    np.array([0,0,0,1])))
+        return T, radii
 
 # ---------------------------------------------------------------------------------------#
 if __name__ == "__main__":
